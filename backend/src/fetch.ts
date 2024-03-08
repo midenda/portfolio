1c24d9dd9c28344e4e5cc75d630f07c9db7a227c
midenda <39438204+midenda@users.noreply.github.com>
2024-03-08 22:15:05 +0000

    Move mkdir from fetch.ts to npm build script

--- backend/src/fetch.ts
+++ backend/src/fetch.ts
@@ -229,7 +229,6 @@ async function fetchShowcase (write: boolean = false)
   {repositories: repositories}
 
   if (write)
-    mkdirSync (CONTENT_ROOT);
     writeFileSync (`${CONTENT_ROOT}/Repositories.json`, JSON.stringify ({repositories: repositories}));
 
   console.log (`\n\x1b[32mSuccessfully fetched showcase\x1b[0m\n`);
